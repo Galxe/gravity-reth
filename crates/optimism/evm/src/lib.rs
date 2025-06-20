7e589aac510aa069a18957b05a362cb8b275369c
nekomoto911 <nekomoto911@gmail.com>
2025-06-20 19:42:36 +0800

    fix ut and integration test

--- crates/optimism/evm/src/lib.rs
+++ crates/optimism/evm/src/lib.rs
@@ -11,7 +11,7 @@
 
 extern crate alloc;
 
-use alloc::sync::Arc;
+use alloc::{boxed::Box, sync::Arc};
 use alloy_consensus::{BlockHeader, Header};
 use alloy_evm::{FromRecoveredTx, FromTxWithEncoded};
 use alloy_op_evm::{block::receipt_builder::OpReceiptBuilder, OpBlockExecutionCtx};
@@ -216,7 +216,7 @@ where
 
     fn parallel_executor<'a, DB: reth_evm::ParallelDatabase + 'a>(
         &self,
-        db: DB,
+        _db: DB,
     ) -> Box<
         dyn reth_evm::parallel_execute::ParallelExecutor<
                 Primitives = Self::Primitives,
