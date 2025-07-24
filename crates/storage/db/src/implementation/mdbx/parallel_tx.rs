d6249692495e354a94de637f4d2a3a7812ab9f92
nekomoto911 <nekomoto911@gmail.com>
2025-07-24 17:45:55 +0800

    improve ParallelTxRO

--- crates/storage/db/src/implementation/mdbx/parallel_tx.rs
+++ crates/storage/db/src/implementation/mdbx/parallel_tx.rs
@@ -6,27 +6,27 @@ use reth_db_api::{
     table::{DupSort, Encode, Table},
     transaction::DbTx,
 };
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc, Condvar, Mutex,
-};
+use std::sync::{Arc, Mutex};
 
 /// A set of read-only transactions that can be used to read data from the database in parallel.
 #[derive(Debug)]
 pub struct ParallelTxRO {
-    is_tx_busy: AtomicBool,
-    pub(super) tx: Tx<RO>, // FIXME: Make it private.
+    inner: Arc<Mutex<Inner>>,
     env: Environment,
     metrics: Option<Arc<DatabaseEnvMetrics>>,
     max_txs: usize,
-    mut_state: Mutex<MutState>,
-    condvar: Condvar,
 }
 
 #[derive(Debug)]
-struct MutState {
+struct Inner {
+    txs: Vec<WrappedTx>,
     num_txs: usize,
-    idle_txs: Vec<Tx<RO>>,
+}
+
+#[derive(Debug)]
+struct WrappedTx {
+    tx: Arc<Tx<RO>>,
+    held_count: usize,
 }
 
 impl ParallelTxRO {
@@ -36,13 +36,13 @@ impl ParallelTxRO {
     ) -> Result<Self, DatabaseError> {
         let tx = Self::create_tx(&env, metrics.clone())?;
         Ok(Self {
-            tx,
-            is_tx_busy: AtomicBool::new(false),
+            inner: Arc::new(Mutex::new(Inner {
+                txs: vec![WrappedTx { tx: Arc::new(tx), held_count: 0 }],
+                num_txs: 1,
+            })),
             max_txs: 8,
             env,
             metrics,
-            mut_state: Mutex::new(MutState { num_txs: 1, idle_txs: vec![] }),
-            condvar: Condvar::new(),
         })
     }
 
@@ -57,78 +57,54 @@ impl ParallelTxRO {
         .map_err(|e| DatabaseError::InitTx(e.into()))
     }
 
-    fn tx_execute<R>(
+    fn execute_tx<R>(
         &self,
-        f: impl FnOnce(&Tx<RO>) -> Result<R, DatabaseError>,
+        f: impl FnOnce(usize, &Tx<RO>) -> Result<R, DatabaseError>,
     ) -> Result<R, DatabaseError> {
-        if self
-            .is_tx_busy
-            .compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire)
-            .is_ok()
-        {
-            // If the tx is not busy, we can execute the function directly.
-            let result = f(&self.tx);
-            self.is_tx_busy.store(false, Ordering::Release);
-            self.condvar.notify_one();
-            return result;
-        }
-
-        let mut state = self.mut_state.lock().unwrap();
-        loop {
-            if !state.idle_txs.is_empty() {
-                // If there are idle txs, we can use one of them.
-                let tx = state.idle_txs.pop().unwrap();
-                drop(state);
-                let result = f(&tx);
-                self.mut_state.lock().unwrap().idle_txs.push(tx);
-                self.condvar.notify_one();
-                return result;
-            }
-
-            if state.num_txs < self.max_txs {
-                // If the number of txs is less than the max number of txs, we can create a new tx.
-                state.num_txs += 1;
-                drop(state);
+        let mut inner = self.inner.lock().unwrap();
+        let (index, tx) = {
+            // Find the tx with the lowest held_count
+            let num_txs = inner.num_txs;
+            let (index, wrapped_tx) =
+                inner.txs.iter_mut().enumerate().min_by_key(|(_, tx)| tx.held_count).unwrap();
+            if wrapped_tx.held_count > 0 && num_txs < self.max_txs {
+                // Create a new tx and add it to the inner txs
+                inner.num_txs += 1;
+                drop(inner);
                 match Self::create_tx(&self.env, self.metrics.clone()) {
                     Ok(tx) => {
-                        let result = f(&tx);
-                        self.mut_state.lock().unwrap().idle_txs.push(tx);
-                        self.condvar.notify_one();
-                        return result;
+                        let tx = Arc::new(tx);
+                        let tx_clone = tx.clone();
+                        let mut inner = self.inner.lock().unwrap();
+                        inner.txs.push(WrappedTx { tx, held_count: 1 });
+                        (inner.txs.len() - 1, tx_clone)
                     }
                     Err(e) => {
-                        self.mut_state.lock().unwrap().num_txs -= 1;
-                        self.condvar.notify_one();
+                        self.inner.lock().unwrap().num_txs -= 1;
                         return Err(e);
                     }
                 }
+            } else {
+                // Use the existing tx
+                wrapped_tx.held_count += 1;
+                let tx = wrapped_tx.tx.clone();
+                drop(inner);
+                (index, tx)
             }
+        };
 
-            drop(state);
-            if self
-                .is_tx_busy
-                .compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire)
-                .is_ok()
-            {
-                // If the tx is not busy, we can execute the function directly.
-                let result = f(&self.tx);
-                self.is_tx_busy.store(false, Ordering::Release);
-                self.condvar.notify_one();
-                return result;
-            }
-
-            state = self.condvar.wait(self.mut_state.lock().unwrap()).unwrap();
-        }
+        let result = f(index, &tx);
+        result
     }
 
     /// Opens a handle to an MDBX database.
     pub fn open_db(&self, name: Option<&str>) -> reth_libmdbx::Result<reth_libmdbx::Database> {
-        self.tx.inner.open_db(name)
+        self.inner.lock().unwrap().txs[0].tx.inner.open_db(name)
     }
 
     /// Retrieves database statistics.
     pub fn db_stat(&self, db: &reth_libmdbx::Database) -> reth_libmdbx::Result<reth_libmdbx::Stat> {
-        self.tx.inner.db_stat(db)
+        self.inner.lock().unwrap().txs[0].tx.inner.db_stat(db)
     }
 
     /// Returns a raw pointer to the MDBX environment.
@@ -149,7 +125,11 @@ impl DbTx for ParallelTxRO {
         &self,
         key: &<T::Key as Encode>::Encoded,
     ) -> Result<Option<T::Value>, DatabaseError> {
-        self.tx_execute(|tx| tx.get_by_encoded_key::<T>(key))
+        self.execute_tx(|idx, tx| {
+            let result = tx.get_by_encoded_key::<T>(key);
+            self.inner.lock().unwrap().txs[idx].held_count -= 1;
+            result
+        })
     }
 
     fn commit(self) -> Result<bool, DatabaseError> {
@@ -162,18 +142,89 @@ impl DbTx for ParallelTxRO {
     }
 
     fn cursor_read<T: Table>(&self) -> Result<Self::Cursor<T>, DatabaseError> {
-        self.tx_execute(|tx| tx.cursor_read::<T>())
+        self.execute_tx(|idx, tx| {
+            let result = tx.cursor_read::<T>();
+            match result {
+                Ok(mut cursor) => {
+                    let inner = self.inner.clone();
+                    cursor.with_drop_fn(Box::new(move |_| {
+                        inner.lock().unwrap().txs[idx].held_count -= 1;
+                    }));
+                    Ok(cursor)
+                }
+                Err(e) => {
+                    self.inner.lock().unwrap().txs[idx].held_count -= 1;
+                    Err(e)
+                }
+            }
+        })
     }
 
     fn cursor_dup_read<T: DupSort>(&self) -> Result<Self::DupCursor<T>, DatabaseError> {
-        self.tx_execute(|tx| tx.cursor_dup_read::<T>())
+        self.execute_tx(|idx, tx| {
+            let result = tx.cursor_dup_read::<T>();
+            match result {
+                Ok(mut cursor) => {
+                    let inner = self.inner.clone();
+                    cursor.with_drop_fn(Box::new(move |_| {
+                        inner.lock().unwrap().txs[idx].held_count -= 1;
+                    }));
+                    Ok(cursor)
+                }
+                Err(e) => {
+                    self.inner.lock().unwrap().txs[idx].held_count -= 1;
+                    Err(e)
+                }
+            }
+        })
     }
 
     fn entries<T: Table>(&self) -> Result<usize, DatabaseError> {
-        self.tx_execute(|tx| tx.entries::<T>())
+        self.execute_tx(|idx, tx| {
+            let result = tx.entries::<T>();
+            self.inner.lock().unwrap().txs[idx].held_count -= 1;
+            result
+        })
     }
 
     fn disable_long_read_transaction_safety(&mut self) {
-        self.tx.disable_long_read_transaction_safety();
+        // Do nothing.
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{mdbx::DatabaseArguments, tables, DatabaseEnv, DatabaseEnvKind};
+    use alloy_consensus::Header;
+    use reth_db_api::{
+        cursor::DbCursorRO,
+        database::Database,
+        models::ClientVersion,
+        transaction::{DbTx, DbTxMut},
+    };
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_parallel_tx_ro() {
+        let dir = tempdir().unwrap();
+        let args = DatabaseArguments::new(ClientVersion::default());
+        let db = DatabaseEnv::open(dir.path(), DatabaseEnvKind::RW, args).unwrap();
+        let tx_rw = db.tx_mut().unwrap();
+        tx_rw.put::<tables::Headers>(0, Header::default()).unwrap();
+        tx_rw.commit().unwrap();
+
+        let tx_ro = db.tx().unwrap();
+        std::thread::scope(|s| {
+            for _ in 0..16 {
+                s.spawn(|| {
+                    for _ in 0..100 {
+                        let mut cursor = tx_ro.cursor_read::<tables::Headers>().unwrap();
+                        let (block_number, header) = cursor.next().unwrap().unwrap();
+                        assert_eq!(block_number, 0);
+                        assert_eq!(header.number, 0);
+                    }
+                });
+            }
+        });
     }
 }
