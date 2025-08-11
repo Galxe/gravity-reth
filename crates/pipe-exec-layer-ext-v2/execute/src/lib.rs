0f722c6b425bd63a0fc480fa8ed1ec99c539bf7f
ByteYue <yj976240184@gmail.com>
2025-08-11 16:54:58 +0800

    for 1.4.8

--- crates/pipe-exec-layer-ext-v2/execute/src/lib.rs
+++ crates/pipe-exec-layer-ext-v2/execute/src/lib.rs
@@ -40,7 +40,9 @@ use reth_provider::{
 };
 
 use revm::{
-    database::{states::bundle_state::BundleRetention, State}, state::AccountInfo, DatabaseCommit
+    database::{states::bundle_state::BundleRetention, State},
+    state::AccountInfo,
+    DatabaseCommit,
 };
 use std::{collections::BTreeMap, sync::Arc, time::Instant};
 
@@ -91,6 +93,8 @@ pub struct OrderedBlock {
     pub transactions: Vec<TransactionSigned>,
     /// Senders of the transactions in the block
     pub senders: Vec<Address>,
+    /// The proposer address of the block
+    pub proposer: Option<Address>,
 }
 
 enum ReceivedBlock {
@@ -509,8 +513,11 @@ impl<Storage: GravityStorage> Core<Storage> {
             // FIXME: This is a hack, we should find a more elegant way to do this
             metadata_evm_env.cfg_env.disable_base_fee = true;
             let mut evm = self.evm_config.evm_with_env(&mut state, metadata_evm_env);
-            let (metadata_txn_result, state_changes) =
-                transact_metadata_contract_call(&mut evm, ordered_block.timestamp * 1_000_000);
+            let (metadata_txn_result, state_changes) = transact_metadata_contract_call(
+                &mut evm,
+                ordered_block.timestamp * 1_000_000,
+                ordered_block.proposer,
+            );
             drop(evm);
 
             if let Some((new_epoch, validators)) = metadata_txn_result.emit_new_epoch() {
