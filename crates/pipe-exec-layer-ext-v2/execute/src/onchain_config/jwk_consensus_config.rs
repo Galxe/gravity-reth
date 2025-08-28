f0e5b159d20dddcb194921e4c4eccf051397e082
AlexYue <yj976240184@gmail.com>
2025-08-28 21:22:47 +0800

    feat: Support fetch jwk onchain (#172)
    * add fetch
    
    * fetch oidc
    
    * update type
    
    * export relayer
    
    * export ObserveState
    
    try to execute
    
    * consider event
    
    * add senders
    
    * use correct nonce for jwk txns
    
    * remove useless log
    
    * little refactor

---
+++ crates/pipe-exec-layer-ext-v2/execute/src/onchain_config/jwk_consensus_config.rs
@@ -0,0 +1,82 @@
+//! Fetcher for consensus configuration
+
+use super::{
+    base::{ConfigFetcher, OnchainConfigFetcher},
+    GRAVITY_FRAMEWORK_ADDRESS, JWK_MANAGER_ADDR,
+};
+use alloy_primitives::{Address, Bytes};
+use alloy_rpc_types_eth::TransactionRequest;
+use alloy_sol_macro::sol;
+use alloy_sol_types::SolCall;
+use reth_rpc_eth_api::{helpers::EthCall, RpcTypes};
+
+sol! {
+    struct OIDCProvider {
+        string name; // Provider name, e.g., "https://accounts.google.com"
+        string configUrl; // OpenID configuration URL
+        bool active; // Whether the provider is active
+    }
+    function getActiveProviders() external view returns (OIDCProvider[] memory);
+}
+
+fn convert_into_bcs_active_providers(oidc_providers: Vec<OIDCProvider>) -> Bytes {
+    let active_providers = oidc_providers
+        .iter()
+        .filter(|provider| provider.active)
+        .map(|provider| gravity_api_types::on_chain_config::jwks::OIDCProvider {
+            name: provider.name.clone(),
+            config_url: provider.configUrl.clone(),
+        })
+        .collect::<Vec<_>>();
+    let jwk_consensus_config = gravity_api_types::on_chain_config::jwks::JWKConsensusConfig {
+        enabled: true,
+        oidc_providers: active_providers,
+    };
+    bcs::to_bytes(&jwk_consensus_config).expect("Failed to serialize JwkConsensusConfig").into()
+}
+
+/// Fetcher for consensus configuration
+#[derive(Debug)]
+pub struct JwkConsensusConfigFetcher<'a, EthApi> {
+    base_fetcher: &'a OnchainConfigFetcher<EthApi>,
+}
+
+impl<'a, EthApi> JwkConsensusConfigFetcher<'a, EthApi>
+where
+    EthApi: EthCall,
+{
+    /// Create a new consensus config fetcher
+    pub const fn new(base_fetcher: &'a OnchainConfigFetcher<EthApi>) -> Self {
+        Self { base_fetcher }
+    }
+}
+
+impl<'a, EthApi> ConfigFetcher for JwkConsensusConfigFetcher<'a, EthApi>
+where
+    EthApi: EthCall,
+    EthApi::NetworkTypes: RpcTypes<TransactionRequest = TransactionRequest>,
+{
+    fn fetch(&self, block_number: u64) -> Bytes {
+        let call = getActiveProvidersCall {};
+        let input: Bytes = call.abi_encode().into();
+
+        let result = self.base_fetcher.eth_call(
+            Self::caller_address(),
+            Self::contract_address(),
+            input,
+            block_number,
+        );
+
+        let solidity_active_providers = getActiveProvidersCall::abi_decode_returns(&result)
+            .expect("Failed to decode getActiveProviders return value");
+        convert_into_bcs_active_providers(solidity_active_providers)
+    }
+
+    fn contract_address() -> Address {
+        JWK_MANAGER_ADDR
+    }
+
+    fn caller_address() -> Address {
+        GRAVITY_FRAMEWORK_ADDRESS
+    }
+}
--- crates/pipe-exec-layer-ext-v2/execute/src/onchain_config/mod.rs
+++ crates/pipe-exec-layer-ext-v2/execute/src/onchain_config/mod.rs
@@ -8,6 +8,8 @@ pub mod epoch;
 pub mod metadata_txn;
 pub mod types;
 pub mod validator_set;
+pub mod observerd_jwk;
+pub mod jwk_consensus_config;
 
 // Re-export main types for convenience
 pub use base::{ConfigFetcher, OnchainConfigFetcher};
